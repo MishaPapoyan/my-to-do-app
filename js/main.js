38477783ecc1adcd1793e0db92ded110e72ba80c
MishaPapoyan <papoyanmisha77@gmail.com>
2023-10-07 14:59:51 +0400

    translate into English

--- js/main.js
+++ js/main.js
@@ -82,7 +82,7 @@ function checEmptyList() {
 		const emptyListHTML = `
 		<li id="emptyList" class="list-group-item empty-list">
 				<img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
-				<div class="empty-list__title">Список дел пуст</div>
+				<div class="empty-list__title">No Tasks</div>
 		</li>`
 		tasksList.insertAdjacentHTML('afterbegin', emptyListHTML)
 	}
